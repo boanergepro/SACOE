ad0df1ad23d6b0e3e90223c5528a9ade97540e74
boanergepro <antonycarrizo96@gmail.com>
2017-08-10 10:05:08 -0400

    [add][mod] Controlador para la autenticacion de los usuarios

---
+++ controladores/auth.js
@@ -0,0 +1,56 @@
+const Strategy = require('passport-local').Strategy;
+
+//MODELOS
+const Usuario = require('../modelos/usuario')
+
+//Estratgia passport
+let estrategia = new Strategy(
+
+	function(username, password, cb) {
+		Usuario.find({
+			where: {
+				username: username
+			}
+		}).then(usuario => {
+			if (usuario){
+				if (usuario.password != password){
+					return cb(null, false)
+				}
+			}else{
+				return cb(null, false)
+			}
+
+			return cb(null, usuario)
+
+		}).catch((err) => {
+			if (err) { return cb(err); }
+		})
+	}
+)
+
+function serializador(user, cb) {
+
+	cb(null, user.id)
+	
+}
+
+function desserializador(id, cb) {
+
+	Usuario.find({
+		where: {
+			id: id
+		}
+	}).then(usuario => {
+		if (usuario) {
+			cb(null, usuario);
+		}
+	}).catch(err => {
+		return cb(err)
+	})
+}
+
+module.exports = {
+	estrategia,
+	serializador,
+	desserializador
+}
