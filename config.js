68bad23de01a7973460b0d70720166f24bc1b099
boanergepro <antonycarrizo96@gmail.com>
2017-11-01 16:41:44 -0400

    [MOD] Añadidas variables para proporcionar información al bot de telegram.

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	port: process.env.PORT || 3001,
-	URI_DB : process.env.DATABASE_URL || 'postgres://postgres:123456@localhost:5432/sacoeDB'
+	URI_DB: process.env.DATABASE_URL || 'postgres://postgres:123456@localhost:5432/sacoeDB',
+	token: '416976554:AAFaGFd_3FpW-GEGPrfN98Uze8ThfdJb8NU',
+	url: 'https://sacoe.herokuapp.com'
 }
