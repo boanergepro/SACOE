58bf90d8b97c7ae69570455d09a1edd8f87b979a
boanergepro <antonycarrizo96@gmail.com>
2017-11-06 16:02:26 -0400

    [ADD] Migracion para la tabla fase_ganar_llamadas.

---
+++ migrations/20171106191228-tabla_fase_ganar_llamadas.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+   
+        return queryInterface.createTable('fase_ganar_llamadas', {
+
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            persona_id: {
+                //chat_id entre el usuario y el bot
+                type: Sequelize.STRING
+                allowNull: false,
+                references: {
+                    model: 'personas',
+                    key: 'id',
+                }
+            },
+            descripcion: Sequelize.STRING,
+            operador: Sequelize.STRING,
+            estado_fase_ganar_llamadas: {
+                type: Sequelize.STRING,
+                set(val) {
+                    this.setDataValue('estado_fase_ganar_llamadas', val.toLowerCase());
+                }
+            }
+        })
+    
+    },
+
+    down: (queryInterface, Sequelize) => {
+    
+        return queryInterface.dropTable('fase_ganar_llamadas')
+    
+    }
+}
