8e0daf7f363cbbca9cb62130d638dd3660ad53b3
boanergepro <antonycarrizo96@gmail.com>
2017-11-06 13:49:49 -0400

    [ADD] Migracion para la tabla notificaciones.

---
+++ migrations/20171106174517-tabla_notificaciones.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+   
+        return queryInterface.createTable('notificaciones', { 
+
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            descripcion: Sequelize.STRING,
+            fecha: {
+                type: Sequelize.DATE, 
+                defaultValue: Sequelize.NOW
+            },
+            categoria: Sequelize.STRING,
+            estado:{
+                type: Sequelize.STRING,
+                defaultValue: 'a'
+            }
+
+        })
+    
+    },
+
+    down: (queryInterface, Sequelize) => {
+    
+        return queryInterface.dropTable('notificaciones')
+    
+    }
+};
