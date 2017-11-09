42f286ca771507efbf48041c74f22f5d4ee10a06
boanergepro <antonycarrizo96@gmail.com>
2017-11-09 14:10:13 -0400

    [MOD] Agregado el autollendo de la tabla heredades.

--- seeders/20171109153617-heredades.js
+++ seeders/20171109153617-heredades.js
@@ -1,26 +1,63 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    up: (queryInterface, Sequelize) => {
 
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
+        return queryInterface.bulkInsert('heredades', [{
+            codigo: 'H1',
+            nombre: 'Adonai',
+            createdAt: 'now()'
+        },{
+            codigo: 'H2',
+            nombre: 'Rafa',
+            createdAt: 'now()'
+        },{
+            codigo: 'H3',
+            nombre: 'Sama',
+            createdAt: 'now()'
+        },{
+            codigo: 'H4',
+            nombre: 'Shalom',
+            createdAt: 'now()'
+        },{
+            codigo: 'H5',
+            nombre: 'Sabaot',
+            createdAt: 'now()'
+        },{
+            codigo: 'H6',
+            nombre: 'Tsidkenu',
+            createdAt: 'now()'
+        },{
+            codigo: 'H7',
+            nombre: 'Elohim',
+            createdAt: 'now()'
+        },{
+            codigo: 'H8',
+            nombre: 'Elyon',
+            createdAt: 'now()'
+        },{
+            codigo: 'H9',
+            nombre: 'Shaddai',
+            createdAt: 'now()'
+        },{
+            codigo: 'H10',
+            nombre: 'Jireh',
+            createdAt: 'now()'
+        },{
+            codigo: 'H11',
+            nombre: 'Raah',
+            createdAt: 'now()'
+        },{
+            codigo: 'H12',
+            nombre: 'Nissi',
+            createdAt: 'now()'
+        }], {});
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+    },
 
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
-  }
-};
+    down: (queryInterface, Sequelize) => {
+
+        return queryInterface.bulkDelete('heredades', null, {});
+
+    }
+}
