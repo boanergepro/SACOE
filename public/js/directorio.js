e78e27561ea130a9c2d01b7b3934f2fac0f25cac
boanergepro <antonycarrizo96@gmail.com>
2017-11-14 13:20:41 -0400

    [ADD][MOD] archivo añadido para usar la logica de enviar el telegram y el atocompletado del input de los contactos.

---
+++ public/js/directorio.js
@@ -0,0 +1,73 @@
+$(document).ready(() => {
+
+/*
+	let url = `${location.origin}/directorio`,
+	let params = {
+		method: "GET"
+	}
+
+	fetch( url, params)
+	    .then( (response) => console.log(response) )
+	    .catch( e => alert("Los datos no han sido envidas") )
+
+*/
+	fetch(`${location.origin}/directorio`, {
+
+		method: 'get'
+
+	}).then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		
+
+		let datos = {}
+		for(let i = 0; i < data.length; i++){
+			if (data[i].telegram_id != null) {
+				datos[`${data[i].nombre} ${data[i].apellido}`] = data[i].telegram_id
+			}
+			 
+		}
+
+		console.log(datos)
+		$('input.autocomplete').autocomplete({
+
+    		data: datos,
+    		limit: 20,
+    		onAutocomplete: function(val) {
+      			
+      			let telegram_id = parseInt(datos[val])
+      			
+      			//guardar el telegram_id en local storage
+      			localStorage.setItem("telegram_id", datos[val])
+      			$("button[type='submit']").click(function(event){
+
+      				//Peticion POST
+					let url = `${location.origin}/enviarTelegram`,
+					params = {
+						method: "post",
+						headers: {
+				        	'Accept': 'application/json',
+				        	'Content-Type': 'application/json'
+				      	},
+						body: JSON.stringify({
+							telegram_id : localStorage.getItem("telegram_id"),
+							persona_id: localStorage.getItem("persona_id")
+						})
+					}
+
+					fetch(url, params).then( (response) => {
+						console.log("Los datos para enviar el telegram han sido enviados")
+					}).catch( e => {
+						console.log("Los datos no han sido envidas") 
+					})
+				})
+			},
+			minLength: 1
+		})
+	}).catch((err) => {
+
+		console.log(err)
+
+	})
+})
