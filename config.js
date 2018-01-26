7a55c10d2bbe96c9ffd800f217a8b92aadbe0a39
boanergepro <antonycarrizo96@gmail.com>
2018-01-26 11:17:58 -0400

    [MOD] Agredados los ; al final de cada linea para estandarizar javascript.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	port: process.env.PORT || 3001,
-	URI_DB: process.env.DATABASE_URL || 'postgres://postgres:123456@localhost:5432/sacoeDB',
-	token: '416976554:AAFaGFd_3FpW-GEGPrfN98Uze8ThfdJb8NU',
-	url: 'https://sacoe.herokuapp.com'
-}
+	URI_DB: process.env.DATABASE_URL || 'postgres://postgres:parangaturimicuaro@localhost:5432/sacoeDB',
+	token: '416976554:AAFaGFd_3FpW-GEGPrfN98Uze8ThfdJb8NU', //telegram token
+	url: 'https://sacoe.herokuapp.com' //url despliegue en heroku
+};
